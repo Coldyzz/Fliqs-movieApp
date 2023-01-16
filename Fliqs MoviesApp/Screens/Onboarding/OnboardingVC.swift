dd5aeb01963de3e7d9e3778508f972f67ec98c4e
Dmytro Kholodok Coldyzz <dkgkpizza@gmail.com>
2023-01-16 21:49:15 +0100

    added onboarding view controller

---
+++ Fliqs MoviesApp/Screens/Onboarding/OnboardingVC.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingVC.swift
+//  Fliqs MoviesApp
+//
+//  Created by Дима Холод on 16.01.2023.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
