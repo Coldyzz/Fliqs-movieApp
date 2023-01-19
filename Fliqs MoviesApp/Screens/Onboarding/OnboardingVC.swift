df34cbaa2cf71b316b40b362fc4163c75df5ed36
Dmytro Kholodok Coldyzz <dkgkpizza@gmail.com>
2023-01-19 23:54:29 +0100

    added extension for collectionViewCell

--- Fliqs MoviesApp/Screens/Onboarding/OnboardingVC.swift
+++ Fliqs MoviesApp/Screens/Onboarding/OnboardingVC.swift
@@ -15,9 +15,36 @@ class OnboardingVC: UIViewController {
     var slides: [OnboardingSlide] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        slides = [OnboardingSlide(title: "Welcome to Fliqs",
+                                  description: "The best movie streaming app of the country to make your days greatfull",
+                                  image: UIImage(named: "LogoApp")!),
+                  OnboardingSlide(title: "Enjoy Your Movies",
+                                  description: "The best movie streaming app of the country to make your days greatfull",
+                                  image: UIImage(named: "LogoApp")!),
+                  OnboardingSlide(title: "Enjoy Your Movies",
+                                  description: "The best movie streaming app of the country to make your days greatfull",
+                                  image: UIImage(named: "LogoApp")!)]
     }
     @IBAction func getStartedClicked(_ sender: UIButton) {
     }
     @IBAction func skipClicked(_ sender: Any) {
+    }
+}
+
+extension OnboardingVC: UICollectionViewDelegate,
+                        UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "onboardingCollectionViewCell",
+                                                      for: indexPath)
+        as! OnboardingCollectionViewCell
+        cell.setup(slides[indexPath.row])
+        return cell
     }
 }
