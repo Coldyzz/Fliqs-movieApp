dd0c176ec4b26fb59bc0c55b3fc66ea71e33a148
Dmytro Kholodok Coldyzz <dkgkpizza@gmail.com>
2023-01-20 20:58:59 +0100

    started to add SignUp ViewController

---
+++ Fliqs MoviesApp/Screens/SignUp/SignUpVC.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpVC.swift
+//  Fliqs MoviesApp
+//
+//  Created by Дима Холод on 20.01.2023.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
